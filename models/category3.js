f5d9cde2ee3faaa7ede618ad261a8e1246aab4de
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-02-20 01:59:38 +1100

    added and updated category 3

---
+++ models/category3.js
@@ -0,0 +1,56 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection')
+
+// Rename Category 3 and category_name
+class Category3 extends Model { }
+
+Category3.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        [category_name]: {
+            // Insert information
+        },
+        [category_name]: {
+            // Insert information
+
+        },
+        [category_name]: {
+            // Insert information
+
+        },
+        [category_name]: {
+            // Insert information
+
+        
+        },
+        [category_name]: {
+            // Insert information
+
+        },
+        // Rename Category 3
+        Category3_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'category1',
+                key: 'id'
+            },
+        },
+    },
+    {
+        sequelize,
+        // timestamps true or false?
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        // Update category1 name
+        modelName: 'Category1',
+    }
+);
+
+// Rename Category3
+module.exports = Category3;
