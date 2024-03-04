767a7825ef5b04d88e0f749a87b0921026d77775
SimpleSuyash <suyashmaharjan@gmail.com>
2024-03-04 21:09:08 +1100

    rating model added

---
+++ models/rating.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Rating extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      defaultValue: 0,
+      validate: {
+        isFloat: true,
+        max: 5,
+        min: 0
+      },
+    },
+
+    brag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'brag',
+        key: 'id',
+      },
+    },
+    rater_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'rating',
+  },
+);
+
+module.exports = Rating;
