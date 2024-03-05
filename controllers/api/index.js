3b43791072db34e8971c3ba8b785a270e361a582
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-03-05 11:43:39 +1100

    fixed index.js in controllers api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 // Import the Express router to create a new router instance
 const router = require('express').Router();
 
+const userRoutes = require('./userRoutes');
 const bragRoutes = require('./bragRoutes');
 const commentRoutes = require('./commentRoutes');
 
+router.use('/user', userRoutes);
 router.use('/brag', bragRoutes);
 router.use('/comment', commentRoutes);
 
