a0fda396bc3494dc3877195d0352837762a57cf5
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-03-08 03:15:45 +1100

    updated comments.js in scripts

--- public/scripts/comment.js
+++ public/scripts/comment.js
@@ -1,8 +1,8 @@
+// Wait for the DOM to be fully loaded before executing the script
 $(window).ready(() => {
   const postBtn = $('#post');
   const commentTxtArea = $('#comment');
-  const cancelBtn =$('.cancel');
-
+  const cancelBtn = $('.cancel');
 
   /*
     const url = window.location.toString();
@@ -12,18 +12,25 @@ $(window).ready(() => {
   //data validation is not done here
   // if user has access to post button
   //that means the comment value is already valid
+
+  // **Post comment handler**
   const postHandler = async (event) => {
+    // Remove leading/trailing whitespace from the comment text
     const comment = $.trim(commentTxtArea.val());
+    // Extract the post ID from the button's data attribute (assuming it exists)
     const postId = $(event.target).data('id');
     try {
+      // Send a POST request to the '/api/comments/' endpoint
       const response = await fetch('/api/comments/', {
         method: 'POST',
         body: JSON.stringify({ content: comment, post_id: postId }),
         headers: { 'Content-Type': 'application/json' },
       });
       if (response.ok) {
+        // If the request is successful, reload the page to display the new comment
         window.location.reload();
       } else {
+        // If the request fails, display an error message
         alert('Failed to create the comment!');
       }
     } catch (error) {
@@ -34,26 +41,32 @@ $(window).ready(() => {
   //when comment is not empty, enable post button
   //when enter key is pressed, create comment
 
+  // **Comment text change handler**
   const textChangeHandler = (event) => {
+    // Enable the post button if the comment text is not empty
     if ($.trim(commentTxtArea.val()) !== '') {
       postBtn.removeAttr('disabled');
+      // If the Enter key is pressed, trigger the postHandler function
       if (event.key === 'Enter') {
         postHandler();
       }
     } else {
+      // Disable the post button if the comment text is empty
       postBtn.attr('disabled', true);
     }
   };
 
-  const cancelHandler =()=>{
-    commentTxtArea.val("");
+  // **Cancel button handler**
+  const cancelHandler = () => {
+    // Clear the comment text area and set focus
+    commentTxtArea.val('');
     commentTxtArea.focus();
+    // Disable the post button
     postBtn.attr('disabled', true);
   };
 
-
+  // Attach event listeners
   postBtn.on('click', postHandler);
   cancelBtn.on('click', cancelHandler);
   commentTxtArea.on('keyup', textChangeHandler);
-
 });
