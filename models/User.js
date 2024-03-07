362a8b70564f182774e92b6d748e35675e94785e
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-03-08 03:07:14 +1100

    updated user.js in models

--- models/User.js
+++ models/User.js
@@ -1,17 +1,23 @@
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
-const { capitalize } = require('../utils/helpers');
-const saltRounds = 3;
+// Import required modules
+const { Model, DataTypes } = require('sequelize'); // Sequelize model and data types
+const bcrypt = require('bcrypt'); // Password hashing library
+const sequelize = require('../config/connection'); // Sequelize connection instance
+const { capitalize } = require('../utils/helpers'); // Utility function for capitalisation
+
+// Define the salt rounds for password hashing
+const saltRounds = 3; // Higher rounds take longer to compute but are more secure
 
 class User extends Model {
+  // Instance method to check a user's password against a provided login password
   checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
+    return bcrypt.compareSync(loginPw, this.password); // Compare hashed password with login password
   }
 }
 
+// Initialise the User model schema
 User.init(
   {
+    // Define model attributes (columns)
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -23,18 +29,18 @@ User.init(
       allowNull: false,
       unique: true,
       validate: {
-        isEmail: true,
-        notNull: true,
-        notEmpty: true,
+        isEmail: true, // Ensure email is a valid email format
+        notNull: true, // Email cannot be null
+        notEmpty: true, // Email cannot be empty
       },
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        notNull: true,
-        notEmpty: true,
-        is: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,32}$/i,
+        notNull: true, // Password cannot be null
+        notEmpty: true, // Password cannot be empty
+        is: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,32}$/i, // Regular expression for password complexity
       },
     },
     username: {
@@ -42,37 +48,45 @@ User.init(
       allowNull: false,
       unique: true,
       validate: {
-        notNull: true,
-        notEmpty: true,
-        isAlphanumeric: true,
+        notNull: true, // Username cannot be null
+        notEmpty: true, // Username cannot be empty
+        isAlphanumeric: true, // Username must only contain letters and numbers
       },
     },
     profile_photo: {
       type: DataTypes.STRING,
-      defaultValue: null,
-    }
+      defaultValue: null, // Allow null value for profile_photo
+    },
   },
   {
+    // Define model hooks (lifecycle events)
     hooks: {
       beforeCreate: async (newUserData) => {
-        //username first letter capitalizing
+        // Capitalise the first letter of the username before creation
         newUserData.username = await capitalize(newUserData.username);
+
         // In this case, we are taking the user's email address, and making all letters lower case before adding it to the database.
+        // Convert the email address to lowercase before storage
         newUserData.email = await newUserData.email.toLowerCase();
-        //storing password in hashed format
+
+        // Storing password in hashed format
+        // Hash the user's password before storing it in the database
         newUserData.password = await bcrypt.hash(
           newUserData.password,
           saltRounds,
         );
-        return newUserData;
+        return newUserData; // Return the modified user data
       },
     },
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user',
+
+    // Define model configuration options
+    sequelize, // Specify the Sequelize connection instance
+    timestamps: false, // Disable automatic timestamp columns (createdAt, updatedAt)
+    freezeTableName: true, // Prevent Sequelize from pluralising the table name
+    underscored: true, // Convert camelCase column names to snake_case
+    modelName: 'user', // Define the model name
   },
 );
 
+// Export the User model
 module.exports = User;
