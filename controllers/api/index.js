875a0da3d2cabacae590caa21c4b4d43a9928f3c
SimpleSuyash <suyashmaharjan@gmail.com>
2024-03-12 10:11:41 +1100

    unneccessay routes removed

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -6,14 +6,14 @@ const userRoutes = require('./user-routes'); // Routes for user-related operatio
 const bragRoutes = require('./brag-routes'); // Routes for brag (post) operations
 const commentRoutes = require('./comment-routes'); // Routes for comment operations
 const ratingRoutes = require('./rating-routes'); // Routes for rating operations
-const uploadRoutes = require('./upload-routes'); // routes for upload operation
+
 
 // Mount the imported route modules onto the main router
 router.use('/users', userRoutes); // Handle user routes under '/users' prefix
 router.use('/brags', bragRoutes); // Handle brag routes under '/brags' prefix
 router.use('/comments', commentRoutes); // Handle comment routes under '/comments' prefix
 router.use('/ratings', ratingRoutes); // Handle rating routes under '/ratings' prefix
-router.use('/upload', uploadRoutes); // Handle upload routes under '/ratings' prefix
+
 
 // Export the router for use in other parts of the application
 module.exports = router;
