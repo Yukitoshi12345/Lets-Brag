44aabd596e9e870a88dc9527cbaa8048c6ad7c26
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-03-11 15:55:42 +1100

    testing heroku

--- config/connection.js
+++ config/connection.js
@@ -4,22 +4,27 @@ const Sequelize = require('sequelize');
 // Use "dotenv" to access environment variables from a `.env` file
 require('dotenv').config();
 
-// Create a Sequelize connection instance
-const sequelize = new Sequelize(
-  // Read database name from the environment variable DB_NAME
-  process.env.DB_NAME,
-  // Read database user from the environment variable DB_USER
-  process.env.DB_USER,
-  // Read database password from the environment variable DB_PASSWORD
-  process.env.DB_PASSWORD,
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // Create a Sequelize connection instance
+  const sequelize = new Sequelize(
+    // Read database name from the environment variable DB_NAME
+    process.env.DB_NAME,
+    // Read database user from the environment variable DB_USER
+    process.env.DB_USER,
+    // Read database password from the environment variable DB_PASSWORD
+    process.env.DB_PASSWORD,
 
-  // Additional connection options
-  {
-    host: 'localhost', // Database host (localhost in this case)
-    dialect: 'mysql', // Database dialect (MySQL in this case)
-    port: 3306, // Database port (default MySQL port)
-  },
-);
+    // Additional connection options
+    {
+      host: 'localhost', // Database host (localhost in this case)
+      dialect: 'mysql', // Database dialect (MySQL in this case)
+      port: 3306, // Database port (default MySQL port)
+    },
+  );
+}
 
 // Export the Sequelize instance for use in other modules
 module.exports = sequelize;
